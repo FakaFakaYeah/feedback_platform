6878b4931e542cafe268f9f3733db55d14682832
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2023-01-16 02:14:53 +0300

    Финальные правки

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -24,8 +24,8 @@ router_v1.register(
 )
 
 urlpatterns = [
+    path('v1/drf-auth/', include('rest_framework.urls')),
     path('v1/auth/token/', get_token, name='get_token'),
     path('v1/auth/signup/', registration, name='signup'),
     path('v1/', include(router_v1.urls)),
-    path('v1/drf-auth/', include('rest_framework.urls')),
 ]
