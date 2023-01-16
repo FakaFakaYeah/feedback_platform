8784c99534cac307608d7f577ecf012361637a45
ÐСтепнан СмирновÑÑ <fakafaka270695@yandex.ru>
2023-01-16 03:03:58 +0300

    Финальные правки

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -129,6 +129,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': [
